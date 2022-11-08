b14b42156e7db491c3aaa0b7c35a266f8eba3269
lisongting <3075272632@qq.com>
2022-11-08 17:46:05 +0800

    增加了展示Setor的主页，添加了Components的部分配置信息

---
+++ src/root.js
@@ -0,0 +1,6 @@
+Setor.components({
+  AppMenu: "src/components/AppMenu",
+  AppAudios: "src/components/AppAudios",
+  AppPackhtml: "src/components/AppPackHtml",
+  AppBase64: "src/components/AppBase64"
+})
